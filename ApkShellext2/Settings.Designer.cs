f76fa7253d6fefbed0b550c1d0e3ba3d9f394372
Jingang Guo, KK <kkguokk@gmail.com>
2015-07-02 01:14:45 -0400

    add setting form

---
+++ ApkShellext2/Settings.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ApkShellext2 {
+    partial class Settings {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            if (getVersionTh.ThreadState != System.Threading.ThreadState.Stopped)
+                getVersionTh.Abort();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(191, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(151, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Rename with VersionCode";
+            this.toolTip1.SetToolTip(this.checkBox1, "When checked, rename apk file as <label>_<versionName>_<versionCode>.apk, \r\nother" +
+        "wise, only <label>_<versionName>.apk");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(282, 132);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(67, 25);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "OK";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUpdate.Location = new System.Drawing.Point(209, 132);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(67, 25);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "GitHub";
+            this.btnUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnUpdate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.btnUpdate, "Go to Project site on Github.");
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.Location = new System.Drawing.Point(9, 138);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(194, 19);
+            this.versionLabel.TabIndex = 4;
+            this.versionLabel.Text = "Great! You are using the latest.";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 0;
+            this.toolTip1.AutoPopDelay = 0;
+            this.toolTip1.InitialDelay = 0;
+            this.toolTip1.ReshowDelay = 110;
+            this.toolTip1.ShowAlways = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(191, 35);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(144, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Always show GooglePlay";
+            this.toolTip1.SetToolTip(this.checkBox2, "If checked, GooglePlay menu will show up even multiple apk files selected. \r\nPlea" +
+        "se note, you may open a lot of window for each selected file.\r\nBy default, only " +
+        "shows one file selected");
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Current Version:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ApkShellext2.Properties.Resources.logo;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 169);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ApkShellext2 Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ApkShellext2/Settings.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using SharpShell.Diagnostics;
+using System.Threading;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+
+namespace ApkShellext2 {
+    public partial class Settings : Form {
+        public Settings() {
+            InitializeComponent();
+        }
+
+        Thread getVersionTh;
+        private void Settings_Load(object sender, EventArgs e) {
+            this.Icon = Icon.FromHandle(Utility.ResizeBitmap(Properties.Resources.logo,16).GetHicon());
+            btnUpdate.Image = Utility.ResizeBitmap(Properties.Resources.GitHub, 16);
+            label1.Text = "Current Version: " + GetType().Assembly.GetName().Version.ToString();
+
+            checkBox1.Checked = (Utility.getRegistrySetting("RenameWithVersionCode") == 1);
+            checkBox2.Checked = (Utility.getRegistrySetting("AlwaysShowGooglePlay") == 1);
+            getVersionTh = new Thread(new ThreadStart(getLatestVersion));
+            getVersionTh.Start();
+        }
+
+        private void getLatestVersion() {
+            try {
+                versionLabel.Text = "Checking newer version...";
+                byte[] buf = new byte[1024];
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://kkguo.github.io/apkshellext/latest");
+                // execute the request
+                HttpWebResponse response = (HttpWebResponse)
+                    request.GetResponse();
+                // we will read data via the response stream
+                Stream resStream = response.GetResponseStream();
+                int count = resStream.Read(buf, 0, buf.Length);
+                string s = "";
+                if (count != 0) {
+                    s = Encoding.ASCII.GetString(buf, 0, count);
+                }                
+                s = Regex.Replace(s, @"\t|\n|\r", "");
+                Logging.Log("Get the latest version :" + s);
+                string[] latestV = s.Split(new Char[] { '.' });
+                string s1 = GetType().Assembly.GetName().Version.ToString();
+                string[] curV = s1.Split(new Char[] { '.' });
+                bool newAvail = false;
+                // version number should be always 4 parts
+                for (int i = 0; i < latestV.Length; i++) {
+                    if (int.Parse(latestV[i]) > int.Parse(curV[i])) {
+                        newAvail = true;
+                        break;
+                    }
+                }
+                if (newAvail) {
+                    versionLabel.Text= "Newer version " + s + " is availible";
+                    btnUpdate.Text = "Update";
+                } else {
+                    versionLabel.Text = "Great! You are using the latest.";
+                    btnUpdate.Text = "GitHub";
+                }
+            } catch (Exception ex) {
+                versionLabel.Text = "Please check the project site for update.";
+                btnUpdate.Text = "GitHub";
+                Logging.Log("Cannot access the web " + ex.Message);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e) {
+            System.Diagnostics.Process.Start("https://kkguo.github.io/apkshellext/index.html?version=" + GetType().Assembly.GetName().Version.ToString());
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            Utility.setRegistrySetting("RenameWithVersionCode", checkBox1.Checked ? 1 : 0);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) {
+            Utility.setRegistrySetting("AlwaysShowGooglePlay", checkBox2.Checked ? 1 : 0);
+        }        
+    }
+}
